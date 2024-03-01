16e237908c992ba8f5b43117750d19ef5f2a515a
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 14:59:41 +0300

    First Commit

---
+++ loops.js
@@ -0,0 +1,8 @@
+// for(Initial value;The loop's continuation condition;The loop's continuation format){
+//             The code that will run as long as the loop continues
+//         }
+
+        for(let i=0, j=0, t=0;i<10, j<=20, t<=30;i++, j+=2, t+=3){
+            console.log(i,j,t)
+            
+        }
